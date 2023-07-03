dd3164796de65dc8eeeb5728b966701cd8d51662
B Marques <brunosmarques1995@hotmail.com>
2023-07-03 20:33:15 -0300

    added mongo header

---
+++ server/include/MongoConn.hpp
@@ -0,0 +1,16 @@
+#ifndef MONGO_CONN_HPP
+#define MONGO_CONN_HPP
+
+namespace ServerMonitor
+{
+    class MongoConn
+    {
+    private:
+        /* data */
+    public:
+        MongoConn(/* args */);
+        ~MongoConn();
+    };
+} // namespace ServerMonitor
+
+#endif // MQTT_CONN_HPP
